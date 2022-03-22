aba2cc3943b9ea90ba634b7a9cb1648cb4031ea4
Pranali <pranalilambat00@gmail.com>
2022-03-22 18:19:24 +0530

    [Pranali]Add:FullFolder

---
+++ IndianStateCensusAnalyzer/CensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using IndianStateCensusAnalyzer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnalyzer
+{
+        public class CensusAnalyser
+        {
+            public enum Country
+            {
+                INDIA,
+                US,
+            }
+
+            Dictionary<string, CensusDTO> dataMap;
+
+            public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+            {
+                dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+                return dataMap;
+            }
+        }
+    }
---
+++ IndianStateCensusAnalyzer/POCO/CensusDataDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnalyzer.POCO
+{
+    public class CensusDataDAO
+    {
+        public string state;
+        public string population;
+        public string area;
+        public string density;
+
+        public CensusDataDAO(string state, string population, string area, string density)
+        {
+            this.state = state;
+            this.population = population;
+            this.area = area;
+            this.density = density;
+        }
+    }
+}
